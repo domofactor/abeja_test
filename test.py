6869a549ddec02af7b2499c88081182da266db20
Terrell Broomer <terrell@moz.com>
2015-05-18 23:19:55 +0900

    adding first pass of script, so far gathers, sorts and serializes into JSON

---
+++ test.py
@@ -0,0 +1,75 @@
+# Abeja Test 201505 by Terrell Broomer
+import requests
+from bs4 import BeautifulSoup
+
+baseurl = "http://www.posh24.com"
+
+def getData(baseurl):
+  r = requests.get(baseurl + "/celebrities/a_to_z")
+  html = r.text
+  parsed_html= BeautifulSoup(html)
+  return parsed_html
+  
+def getCelebPath(parsed_html):
+  celeb_dict = {}
+  celebs = parsed_html.body.find_all('div', attrs={'class':'col-xs-6 col-md-4'})
+
+  for celeb in celebs:
+    celeb_link = celeb.find_all('a')
+    for link in celeb_link:
+      name = link.text
+      path = link.get('href')
+      celeb_dict[name] = {}
+      celeb_dict[name]['path'] = path
+  return celeb_dict
+
+def getCelebData(celeb):
+  celeb_path = celeb_dict[celeb]['path']
+  r = requests.get(baseurl + celeb_path)
+  html = r.text
+  celeb_html = BeautifulSoup(html)
+  return celeb_html
+
+def parseCelebHTML(celeb,celeb_html):
+  celeb_thumb = celeb_html.find('img')['src']
+  celeb_dict[celeb]['thumb'] = celeb_thumb
+
+  celeb_desc = celeb_html.find('div', attrs={'class':'info x-readmore'}).text
+  celeb_dict[celeb]['desc'] = celeb_desc
+
+  celeb_born = celeb_html.find('div', attrs={'class':'attributeTitle birth','class':'attributeContent'}).text
+  celeb_dict[celeb]['born'] = celeb_born
+
+  celeb_age_str = celeb_html.find_all('div', attrs={'class':'attributeTitle age','class':'attributeContent'})[-2].getText()
+  celeb_age = int(celeb_age_str)
+  celeb_dict[celeb]['age'] = celeb_age
+
+  return celeb_dict[celeb]
+
+def formatCelebData(celeb_data):
+  import json
+  print json.dumps(celeb_data)
+
+def insetCelebJSON(celeb_json):
+  
+  
+
+########
+# Main #
+########
+
+parsed_html = getData(baseurl)
+celeb_dict = getCelebPath(parsed_html)
+
+
+for celeb in celeb_dict:
+  if 'Alexandra Burke' in celeb:
+    print('Celeb Found')
+    break
+  else:
+    celeb_html = getCelebData(celeb)
+    celeb_data = parseCelebHTML(celeb,celeb_html)
+
+celeb_json = formatCelebData(celeb_data)
+
+insertCelebJSON(celeb_json)
